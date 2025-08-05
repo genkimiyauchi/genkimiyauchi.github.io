7d7099214fa72c41ac9bc57547d6528137bbbd69
genkimiyauchi <genkimiyauchi@icloud.com>
2025-08-05 13:25:27 +0000

    Deploying to gh-pages from @ genkimiyauchi/genkimiyauchi.github.io@092756b351a3a72e0688191a65631a495d3ddb82 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/news/";
           },
+        },{id: "nav-japanese-日本語",
+          title: "Japanese (日本語)",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/ja";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
